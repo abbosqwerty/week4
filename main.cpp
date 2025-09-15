3fdd805efe72a6a6c79f1bf96bbf3dca02754db5
abbosqwerty <abbosqurbonov492@gmail.com>
2025-09-15 12:24:44 +0500

    first

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+// int main() {
+//     /* Selection control Structures if, if-else, nested if-else
+//      * if(condition ){it code we write after the condition}
+//      * else(we have to not write condition to else ){ there we write code for else }
+//      */
+// }
+
+/*Problem #1
+int main() {
+    int x;
+    cout << "Please enter a number: ";
+    cin >> x;
+    if (x>0) {
+        cout <<"Your number is positive:";
+        return 0;
+    }
+    else {
+        cout << "Your number is negative:";
+        return 1;
+    }
+}*/
+int main() {
+
+}
