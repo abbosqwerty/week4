f95c6a630d829c9e30c86f370d6d2517f76eea3d
abbosqwerty <abbosqurbonov492@gmail.com>
2025-09-17 12:46:45 +0500

    second lab

--- main.cpp
+++ main.cpp
@@ -1,227 +1,194 @@
+#include <chrono>
 #include <iostream>
+#include <math.h>
+#include <cmath>
 using namespace std;
-// int main() {
-//     /* Selection control Structures if, if-else, nested if-else
-//      * if(condition ){it code we write after the condition}
-//      * else(we have to not write condition to else ){ there we write code for else }
-//      */
-// }
 
-/*Problem #1
+/* Problem #10
+
 int main() {
-    int x;
-    cout << "Please enter a number: ";
+    char c;
+    cout<<"Enter a character: ";
+    cin>>c;
+    switch(c) {
+        case 'g':
+            cout<<"Goo";
+            break;
+        case 'y':
+            cout<<"Get ready";
+            break;
+        case 'r':
+            cout<<"Stop";
+            break;
+        default:
+            cout<<"Wrong input";
+    }
+}
+*/
+
+
+/* Problem #11
+
+int main() {
+        int n;
+    cout << "Enter the student score (0-100): ";
+    cin >> n;
+    switch(n/10) {
+        case 10:
+        case 9:
+            cout <<"A";
+            break;
+        case 8:
+            cout <<"B";
+            break;
+        case 7:
+            cout <<"C";
+            break;
+        case 6:
+            cout <<"D";
+            break;
+        default:
+            cout <<"F";
+    }
+} */
+
+/* Problem 12
+
+int main() {
+    int x, y,z;
+    cout << "Please enter each angles of triangel: " ;
+    cin >> x >> y >> z;
+    switch (x+y+z) {
+        case 180:
+         cout <<"the triangle is valid";
+        break;
+        default:
+            cout <<"the triangle is not valid";
+    }
+} */
+
+
+/* Problem 13
+
+int main() {
+    double a, b, c;
+    cout << "Please enter the first number: ";
+    cin >> a;
+    cout << "Please enter the second number: ";
+    cin >> b;
+    cout << "Please enter the third number: ";
+    cin >> c;
+    double dis = (b * b) - (4 * a * c);
+        double x1 = (-b + sqrt(dis)) / (2 * a);
+        double x2 = (-b - sqrt(dis)) / (2 * a);
+        cout <<"x1 = "<< x1 << endl;
+        cout <<"x2 = "<< x2 << endl;
+
+} */
+
+/* Problem 14
+
+int main() {
+    char c;
+    cout << "Please enter a character: ";
+    cin >> c;
+    if(c >= 'a' && c <= 'z') {
+        cout <<"Lowercase Alphabet";
+
+    }
+    else if(c >= 'A' && c <= 'Z') {
+        cout <<"Uppercase Alphabet";
+
+    }
+    else {
+        cout <<"it is not an alphabet";
+    }
+} */
+
+/* Problem 15
+
+int main() {
+    double x,y,z,u;
+    cout << "Enter weight and price for package";
+    cin >> x >> y;
+    cout << "Enter weight and price for 2nd package";
+    cin >> z >> u;
+    if (x/y > z/u) {
+        cout << "Package 1 has better price";
+    }
+    else if (x/y < z/u) {
+        cout << "Package 2 has better price";
+    }
+    else {
+        cout << "two packages have the same price";
+    }
+
+} */
+
+/* Problem 16
+
+int main() {
+      int x,y,z, num;
+    cout << "Enter a three-digit integer";
+    cin >> num;
+    x = num % 10;
+    z = (num / 10) % 10;
+    y = num / 100;
+    if (x == y) {
+        cout << num <<" is  a palindirome";
+    }
+    else {
+        cout << num <<" is not a palindirome";
+    }
+
+
+} */
+/* Problem 17
+
+int main() {
+    char greet;
+    cout<< " Enter character to greets ";
+    cin >> greet;
+    switch (greet) {
+        case 'u':
+            cout << "Assalomu Aleykum";
+            break;
+        case 'e':
+            cout << "Hello";
+            break;
+        case 'r':
+            cout << "Zdravstvuyte";
+            break;
+        case 'g':
+            cout << "Hallo";
+            break;
+        default:
+            cout << " I dont know this language";
+    }
+} */
+
+/* Problem 18
+
+int main() {
+    double x,y,d;
+    cout << "Enter x coordinate: ";
     cin >> x;
-    if (x>0) {
-        cout <<"Your number is positive:";
-        return 0;
+    cout << "Enter y coordinate: ";
+    cin >> y;
+    d = sqrt(pow(x,2) + pow(y,2));
+    if (d < 10) {
+        cout <<" the point ("<<x<<", "<<y<<") is inside the circle"<<endl;
+    }
+    else if (d == 10 ) {
+        cout <<" the point ("<< x<<", "<<y<<") is on the circle"<<endl;
     }
     else {
-        cout << "Your number is negative:";
-        return 1;
+        cout <<" the point ("<< x<<", "<<y<<") is outside the circle"<<endl;
+
     }
-}*/
-
-
-/*
-Problem #2
+} */
+/* Problem 18
+ */
 int main() {
- int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    if (n%2==0) {
-        cout << "The number is even" << endl;
-        return 0;
-
-    }
-    else {
-        cout << "The number is odd" << endl;
-        return 0;
-    }
-}
-*/
-
-
-/* Problem #3
-int main() {
-    int x,y max;
-     cout<<"Enter the value of x: ";
-     cin>>x;
-     cout<<"Enter the value of y : ";
-     cin>>y;
-     if(x==y){
-           cout << "Numbers are equal" ;
-     }
-     else if (x>y) {
-        max = x;
-
-     }
-     else {
-         max = y;
-         return 0;
-     }
-       cout<<"Max number is: "<<max<<endl;
-}
-*/
-
-/* Problem #4
-int main() {
-    int x,y,z, max;
-    cout<<"Enter the first number: ";
-    cin>>x;
-    cout<<"Enter the second number: ";
-    cin>>y;
-    cout<<"Enter the third number: ";
-    cin>>z;
-    if(x>y) {
-        if(x>z) {
-            max=x;
-        }
-        else {
-            max=z;
-        }
-    }
-    else {
-        if(y>z) {
-            max=y;
-        }
-        else {
-            max=z;
-        }
-    }
-    cout<<"Max number is: "<<max<<endl;
-}
-*/
-
-/* Problem #5
-int main() {
-    int x;
-    cout << "Please enter a year: ";
-    cin >> x;
-
-    if ( x%4==0) {
-        if ( x%100==0) {
-            if ( x%400==0) {
-                cout<<x<<" is a leap year"<<endl;
-            }
-            else {
-                cout<<x<<" is not a leap year"<<endl;
-            }
-        }
-        else {
-            cout<<x<<" is a leap year"<<endl;
-        }
-    }
-    else {
-        cout<<x<<" is not a leap year"<<endl;
-    }
-}
-*/
-
-/*Problem #6
-int main() {
-    int n;
-    cout << "Enter your score:";
-    cin >> n;
-    if (n>=50) {
-        cout << "Pass";
-
-    }
-    else {
-        cout << "Fail";
-    }
-}
-*/
-
-/* Probelem #7
-int main() {
-    int n;
-
-    cout << "Enter a number: ";
-    cin >> n;
-    if (n>=0) {
-        if (n==0) {
-            cout << "The number is 0" << endl;
-        }
-        else {
-            cout << "Positive value!";
-        }
-    }
-    else {
-        cout << "Negative number!" << endl;
-    }
 
 }
-*/
-
-
-/* Problem #8
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-     int ab = (n)? -n : n;
-    cout <<"the absolute value is: " << ab << endl;
-}
-*/
-
-/* Problem #12
-int main() {
-    int n;
-    cout << "enter your score:";
-    cin >> n;
-    if (n>=60) {
-        if (n>=70) {
-            if (n>=80) {
-                if (n>=90) {
-                    cout <<"Your grade is A";
-                }
-                else {
-                    cout <<"Your grade is B";
-
-                }
-            }
-            else {
-                cout <<"Your grade is C";
-            }
-        }
-        else {
-            cout <<"Your grade is D";
-        }
-    }
-    else {
-        cout <<"Your grade is F";
-    }
-}
-*/
-
-
-/* Problem #16
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    if (n%3 == 0 && n%5 == 0) {
-        cout << "Divisible by both" << endl;
-    }
-    else {
-        cout << "Not divisible by both" << endl;
-    }
-}
-*/
-
-/* Problem #20
-int main() {
-    int a,b;
-    cout<<"Enter two numbers: ";
-    cin>>a>>b;
-   int rem = a%b;
-   if (b==0) {
-       cout <<"Number a is  not devisibel by number b";
-   }
-    else {
-        cout <<"Number a is  devisibel by number b" <<" and remineder is: " <<rem;
-    }
-
-}
-*/
